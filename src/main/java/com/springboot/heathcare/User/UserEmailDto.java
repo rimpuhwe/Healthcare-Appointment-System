29e4841fe1899213b6d120c95c3cd01f1875c6a3
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-06-03 11:30:12 +0200

    chore: add the email dto for email update

--- src/main/java/com/springboot/heathcare/User/UserEmailDto.java
+++ src/main/java/com/springboot/heathcare/User/UserEmailDto.java
@@ -1,7 +1,22 @@
-package com.springboot.heathcare;
+package com.springboot.heathcare.User;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+
+import java.io.Serializable;
 
 /**
  * DTO for {@link com.springboot.heathcare.User.User}
  */
-public class UserDto {
-  }
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEmailDto implements Serializable {
+    @Email(message = "provide a valid email", regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\\\.[a-zA-Z]{2,}$")
+    @NotBlank(message = "Email field is required")
+    String email;
+}
