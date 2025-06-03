56b7646fcd3a67a257655c03743a1e010aec555b
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-06-03 11:29:14 +0200

    chore: add the all activities user can perform in Userservice class

---
+++ src/main/java/com/springboot/heathcare/User/UserPasswordDto.java
@@ -0,0 +1,7 @@
+package com.springboot.heathcare;
+
+/**
+ * DTO for {@link com.springboot.heathcare.User.User}
+ */
+public class UserPasswordDto {
+  }
---
+++ src/main/java/com/springboot/heathcare/User/UserService.java
@@ -0,0 +1,31 @@
+package com.springboot.heathcare.User;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+    public User updateUserEmail(@PathVariable String firstName , @RequestBody UserEmailDto userEmailDto) {
+        User user = new User();
+        user.setEmail(userEmailDto.getEmail());
+        return userRepository.save(user);
+    }
+    public User updateUserPassword(@PathVariable String firstName , @RequestBody UserPasswordDto userPasswordDto) {
+        User user = new User();
+        user.setPassword(userPasswordDto.getPassword());
+        return userRepository.save(user);
+    }
+    public void deleteUser(@PathVariable Long id) {
+        var user = userRepository.findById(id).orElseThrow(()-> new RuntimeException("User doesn't exist"));
+        userRepository.delete(user);
+    }
+}
