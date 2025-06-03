3e5b321de109807a53777f4a511d5fb3e255fefc
hopegaju <hopegajupr@gmail.com>
2025-06-03 16:15:37 +0200

    Feat: Add LoginDto for user authentication

--- src/main/java/com/springboot/heathcare/User/LoginDto.java
+++ src/main/java/com/springboot/heathcare/User/LoginDto.java
@@ -1,4 +1,13 @@
 package com.springboot.heathcare.User;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class LoginDto {
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
 }
