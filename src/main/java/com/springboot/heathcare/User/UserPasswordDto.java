a6e363d3dd8f106a55d0e4bb328097202d1cf9c4
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-06-03 11:32:24 +0200

    chore: add the password dto for email update

--- src/main/java/com/springboot/heathcare/User/UserPasswordDto.java
+++ src/main/java/com/springboot/heathcare/User/UserPasswordDto.java
@@ -1,7 +1,20 @@
-package com.springboot.heathcare;
+package com.springboot.heathcare.User;
+
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+
+import java.io.Serializable;
 
 /**
  * DTO for {@link com.springboot.heathcare.User.User}
  */
-public class UserPasswordDto {
-  }
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserPasswordDto implements Serializable {
+    @Pattern(message = "choose the strongest password", regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$ ")
+    String password;
+}
