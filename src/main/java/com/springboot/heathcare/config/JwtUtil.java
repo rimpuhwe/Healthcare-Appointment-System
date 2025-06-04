c02bab23c92f37d97fad689ae864e81423374eaf
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-06-04 12:11:49 +0200

    unknown

--- src/main/java/com/springboot/heathcare/config/JwtUtil.java
+++ src/main/java/com/springboot/heathcare/config/JwtUtil.java
@@ -1,4 +1,9 @@
 package com.springboot.heathcare.config;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class JwtUtil {
+    public generateToken(@NotBlank String email) {
+        return null;
+    }
 }
