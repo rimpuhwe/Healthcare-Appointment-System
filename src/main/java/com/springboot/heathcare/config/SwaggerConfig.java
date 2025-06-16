b69d535a4abdef8c015dd024bb954b368a25b36e
hopegaju <hopegajupr@gmail.com>
2025-06-16 10:12:44 +0200

    feat: configure OpenAPI with bearer token authentication

--- src/main/java/com/springboot/heathcare/config/SwaggerConfig.java
+++ src/main/java/com/springboot/heathcare/config/SwaggerConfig.java
@@ -1,4 +1,20 @@
 package com.springboot.heathcare.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+
+@SecurityScheme(
+        type = SecuritySchemeType.HTTP,
+        name = "auth",
+        description = "JWT Authentication",
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+@Configuration
 public class SwaggerConfig {
+
 }
